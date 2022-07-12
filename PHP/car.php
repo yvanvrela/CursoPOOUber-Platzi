a57e9049c5dfe76f5271a6bbff119ef3cc1ab507
Yvan <varelaivan30@gmail.com>
2022-07-11 20:42:26 -0400

    added php constructor in car and account

--- PHP/car.php
+++ PHP/car.php
@@ -5,4 +5,15 @@ class Car
     public $license;
     public $driver;
     public $passegenger;
+
+    public function __construct($license, $driver)
+    {
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    public function printDataCar()
+    {
+        echo "license: $this->license, conductor: {$this->driver->name}, document: {$this->driver->document}";
+    }
 }
