ac155fbf5bbbc3c40b7440655f7412ad4f1ebece
Yvan <varelaivan30@gmail.com>
2022-07-14 18:07:41 -0400

    added more variables in tarjeta

--- Python/tarjeta.py
+++ Python/tarjeta.py
@@ -2,5 +2,12 @@ from payment import Payment
 
 
 class Tarjeta(Payment):
-    def __init__(self) -> None:
+    number = str
+    cvv = str
+    date = str
+
+    def __init__(self, number, cvv, date) -> None:
         super().__init__()
+        self.number = number
+        self.cvv = cvv
+        self.date = date
