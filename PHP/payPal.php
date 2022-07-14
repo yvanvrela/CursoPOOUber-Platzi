2222dbe19cdb24bd703510bdfe6be991f0ab0d45
Yvan <varelaivan30@gmail.com>
2022-07-14 18:12:43 -0400

    added construct whit variables

--- PHP/payPal.php
+++ PHP/payPal.php
@@ -2,4 +2,10 @@
 require_once('payment.php');
 class PayPal extends Payment
 {
+    public $email;
+
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
 }
--- PHP/tarjeta.php
+++ PHP/tarjeta.php
@@ -2,4 +2,14 @@
 require_once('payment.php');
 class Tarjeta extends Payment
 {
+    public $number;
+    public $cvv;
+    public $date;
+
+    public function __construct($number, $cvv, $date)
+    {
+        $this->number = $number;
+        $this->cvv = $cvv;
+        $this->date = $date;
+    }
 }
