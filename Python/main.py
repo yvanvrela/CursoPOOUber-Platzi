006860361e7582c7b3f09019a214f73c2557761a
Yvan <varelaivan30@gmail.com>
2022-07-14 17:19:56 -0400

    applied heritange of uberBlack

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,20 @@
 from account import Account
 from car import Car
+from uberBlack import UberBlack
 from uberX import UberX
 
 
 def main() -> None:
 
-    uberX = UberX("AGW342", Account(
-        "Yvan Varela", "DRE323"), "Hyundai", "Tucson")
+    uberX = UberX('AGW342', Account(
+        'Yvan Varela', 'DRE323'), 'Hyundai', 'Tucson')
 
     print(vars(uberX))
+
+    uberBlack = UberBlack('GHF342', Account(
+        'Martin', 'ERT323'), ['Buggatti', ], ['Cuero', 'alcantra'])
+
+    print(vars(uberBlack))
 
 
 if __name__ == "__main__":
